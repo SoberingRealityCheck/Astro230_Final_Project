e28a7a06b06c8ca90c973ad10860895e9a168a10
Clay G <buzzscoutbark@gmail.com>
2025-04-22 23:58:19 -0500

    neat little density vs cluster center distance chart for stuff within 1 sigma of median

--- generate_3d_plot.py
+++ generate_3d_plot.py
@@ -88,6 +88,27 @@ def extract_star_positions(table):
             print("type of c[i]", type(c[i]))
             print("c[i]", c[i])
             c[i] = (int(abs(c[i])) / 4)
+    
+    # Let's make a cool graph displaying density of stars vs radial distance
+    definitive_cluster_members = []
+    for i in range(len(distance)):
+        if distance[i] < mu + std and distance[i] > mu - std:
+            definitive_cluster_members.append(distance[i])
+    
+    cluster_center = np.nanmedian(definitive_cluster_members)
+    print("Cluster Center:", cluster_center)
+    
+    cluster_radii = []
+    for i in range(len(distance)):
+        if distance[i] < mu + std and distance[i] > mu - std:
+            cluster_radii.append(abs(distance[i] - cluster_center))
+    
+    plt.hist(cluster_radii, bins=100)
+    plt.xlabel('Radial Distance from Cluster Center (parsecs)')
+    plt.ylabel('Star Density')
+    plt.title('Star Density vs Radial Distance from Cluster Center')
+    plt.show()
+    
 
 
     return np.array([x, y, z, c]).T
@@ -95,7 +116,7 @@ star_table = Table.read(path + '../star_matching/target_table.tex')
 star_positions = extract_star_positions(star_table)
 print("Star Positions (3D):", star_positions)
 
-print("Median Star Position:", np.median(star_positions, axis=0))
+print("Median Star Position:", np.nanmedian(star_positions, axis=0))
 
 # Dark mode plots because hell yeah
 with plt.style.context('dark_background'):
@@ -113,12 +134,12 @@ with plt.style.context('dark_background'):
     ax.set_title('3D Plot of Star Positions')
     
     # Scatter plot of star positions
-    ax.scatter(star_positions[:, 0], star_positions[:, 1], star_positions[:, 2], marker='o', c = star_positions[:, 3], s=1, alpha=1)
+    ax.scatter(star_positions[:, 0], star_positions[:, 1], star_positions[:, 2], marker='o', c = star_positions[:, 3], s=.1, alpha=1)
     ax.scatter(0, 0, 0, marker='o', color='yellow', s=10)  # Our Sun is at the origin
     
     # Set the limits for each axis to be, like, reasonable
     ax.set_xlim3d(-139,-138)
-    ax.set_ylim3d(677,678)
+    ax.set_ylim3d(678,679)
     ax.set_zlim3d(-263, -262)
     
     # Set the initial viewing angle to be pretty close to our original telescope angle
